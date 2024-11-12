9ef2ce20b4d6a91850ba18a2a8e27ced3659c5b5
Gapord <gapord879@gmail.com>
2024-11-12 21:54:27 +0300

    dell cmd

--- main.py
+++ main.py
@@ -13,24 +13,7 @@ bot = commands.Bot(
 )
 
 
-@bot.command()
-async def devlog(ctx):
-    channel = bot.get_channel(await getchan(ctx.guild.id))
-    if channel is None:
-        await ctx.reply("Канал не найден.")
-        return
 
-    overwrite = channel.overwrites_for(ctx.author)
-    if overwrite.view_channel:
-
-        overwrite.view_channel = False
-        await channel.set_permissions(ctx.author, overwrite=overwrite)
-        await ctx.reply(f"Доступ к каналу для {ctx.author.mention} удалён.")
-    else:
-        overwrite.view_channel = True
-        await channel.set_permissions(ctx.author, overwrite=overwrite)
-        await ctx.reply(f"{ctx.author.mention} получил доступ к каналу.")
-    await ctx.message.delete()
 
 
 @bot.event
