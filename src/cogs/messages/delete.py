0364fe7077fc47f0c91607dffa0dce1c4a73b733
Gapord <gapord879@gmail.com>
2025-08-30 12:03:35 +0300

    Улучшение логгирования сообщений

--- src/cogs/messages/delete.py
+++ src/cogs/messages/delete.py
@@ -1,3 +1,5 @@
+import io
+
 import disnake
 from disnake.ext import commands
 
@@ -10,17 +12,18 @@ class DeleteMSG(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: disnake.Message):
+        deleter = message.author
         async for entry in message.guild.audit_logs(
-            action=disnake.AuditLogAction.message_delete, limit=1
+            action=disnake.AuditLogAction.message_delete, limit=5
         ):
             if entry.target.id == message.author.id:
-                deleter = entry.user
-                break
-        else:
-            deleter = message.author
+                time_diff = (message.created_at - entry.created_at).total_seconds()
+                if abs(time_diff) < 5:
+                    deleter = entry.user
+                    break
 
         guild_data = await self.bot.db.fetch_guild_data(message.guild.id)
-        if guild_data is None:
+        if not guild_data:
             return
 
         log = self.bot.get_channel(guild_data.channel_id)
@@ -28,14 +31,26 @@ class DeleteMSG(commands.Cog):
         embed = disnake.Embed(title="Удаление сообщения", color=guild_data.color)
         embed.add_field(name="Автор", value=message.author.mention)
         embed.add_field(name="Канал", value=message.channel.mention)
-        embed.add_field(name="Сообщение", value=message.content, inline=False)
         embed.set_footer(text=f"Удалил {deleter}")
 
-        if message.attachments:
-            files = [await attachment.to_file() for attachment in message.attachments]
+        files = (
+            [await attachment.to_file() for attachment in message.attachments]
+            if message.attachments
+            else []
+        )
+
+        if len(message.content) > 500:
+            txt_file = disnake.File(
+                fp=io.StringIO(message.content), filename=f"message_{message.id}.txt"
+            )
+            files.append(txt_file)
             await log.send(embed=embed, files=files)
         else:
-            await log.send(embed=embed)
+            if message.content != "":
+                embed.add_field(
+                    name="Сообщение", value=f"```{message.content}```", inline=False
+                )
+            await log.send(embed=embed, files=files if files else None)
 
 
 def setup(bot: CustomClient):
--- src/cogs/messages/edit.py
+++ src/cogs/messages/edit.py
@@ -1,3 +1,5 @@
+import io
+
 import disnake
 from disnake.ext import commands
 
@@ -10,6 +12,8 @@ class EditMSG(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_edit(self, before: disnake.Message, after: disnake.Message):
+        if not before.guild:
+            return
         guild_data = await self.bot.db.fetch_guild_data(after.guild.id)
         if guild_data is None:
             return
@@ -22,9 +26,31 @@ class EditMSG(commands.Cog):
         )
         embed.add_field(name="Автор", value=after.author.mention)
         embed.add_field(name="Канал", value=after.channel.mention)
-        embed.add_field(name="Сообщение до", value=before.content, inline=False)
-        embed.add_field(name="Сообщение после", value=after.content)
-        await log.send(embed=embed)
+        files = (
+            [await att.to_file() for att in before.attachments + after.attachments]
+            if (before.attachments or after.attachments)
+            else []
+        )
+
+        def prepare_text_file(label, text):
+            if not text:
+                return None
+            if len(text) > 500:
+                file = io.StringIO(text)
+                f = disnake.File(fp=file, filename=f"{label}_{before.id}.txt")
+                files.append(f)
+                return None
+            return f"```{text}```"
+
+        before_field = prepare_text_file("before", before.content)
+        after_field = prepare_text_file("after", after.content)
+
+        if before_field and before_field != "":
+            embed.add_field(name="Сообщение до", value=before_field, inline=False)
+        if after_field and after_field != "":
+            embed.add_field(name="Сообщение после", value=after_field, inline=False)
+
+        await log.send(embed=embed, files=files if files else None)
 
 
 def setup(bot: CustomClient):
